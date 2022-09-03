40ec7d62cff9acf325b9769a3c140fb6cd970585
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-09-04 04:49:10 +0600

    chore(database): drop join, leave and media values from activity_type enum

---
+++ prisma/migrations/20220903224642_drop_values_join_leave_from_enum_activity_type/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [join,leave] on the enum `activity_type` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "activity_type_new" AS ENUM ('file', 'text', 'media', 'call', 'other');
+ALTER TABLE "activities" ALTER COLUMN "activity_type" TYPE "activity_type_new" USING ("activity_type"::text::"activity_type_new");
+ALTER TYPE "activity_type" RENAME TO "activity_type_old";
+ALTER TYPE "activity_type_new" RENAME TO "activity_type";
+DROP TYPE "activity_type_old";
+COMMIT;
