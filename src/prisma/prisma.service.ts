5cad9468c2dd84141e329d87facb12d91c6621c7
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-09-11 13:05:45 +0600

    chore: merge postgres module into prisma module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,25 @@
 import { INestApplicationContext, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../prisma/client/index.js';
+import { Config } from '../config/config.js';
+import postgres from 'postgres';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private readonly config: Config) {
+    super();
+  }
+
+  public readonly postgres = postgres(this.config.postgres.url, {
+    publications: this.config.postgres.publications,
+  });
+
   async onModuleInit() {
     await this.$connect();
   }
 
   async enableShutdownHooks(app: INestApplicationContext) {
     this.$on('beforeExit', async () => {
+      await this.postgres.end({ timeout: 10 });
       await app.close();
     });
   }
